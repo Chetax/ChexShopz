65a8ef7e013544930c008da63bfedbec457ba9da
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-02-18 20:05:05 +0530

    Added Cart Section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,20 @@
+import { useSelector } from "react-redux";
+import './Cart.css';
+import { NavLink } from "react-router-dom";
 function Cart() {
-    return ( <>
-    Cart
+const cart=useSelector((state)=>state);
+return ( <>
+
+    {
+        cart.length > 0 ?     <div className="row">
+        <div className="col-6"></div>
+        <div className="col-6"></div>
+    </div> :
+    <div className="Nodataincart p-5 " >
+        <p>No Data Found</p>
+       <NavLink to="/"> <button className="btn btn-primary">Shop Now</button></NavLink>
+    </div> 
+    }
     </>);
 }
 
