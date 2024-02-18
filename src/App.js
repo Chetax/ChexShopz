a4d8d2d8f0b31d37b8a6486cbb2ed90d6989b398
Chetan Navnath Padhen <padhenchetan@gmail.com>
2024-02-18 18:30:19 +0530

    Removed Error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { Routes,Route } from 'react-router-dom';
+
 import './App.css';
-import MainNavbar from './components/MainNavbar';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-   <MainNavbar/>
-  <Routes>
-    <Route path="/" element={Home}/>
-    <Route path="/cart" element={Cart}/>
-  </Routes>
+  
     </div>
   );
 }
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { CartSlice } from './Slices/CartSlice'
-export default configureStore({
-  reducer: {
-    cart: CartSlice,
-  }
-})
